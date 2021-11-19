6231b9e47e2b334cc525bcb3dc39145144855025
vladismeow <vladismeow@gmail.com>
2021-11-19 22:54:14 +0200

    added top slider

--- gulpfile.js
+++ gulpfile.js
@@ -9,70 +9,71 @@ const imagemin = require('gulp-imagemin');
 const browserSync = require('browser-sync').create();
 
 function browsersync() {
-   browserSync.init({
-      server: {
-         baseDir: 'app/'
-      },
-      notify: false
-   })
+	browserSync.init({
+		server: {
+			baseDir: 'app/'
+		},
+		notify: false
+	})
 }
 
 function styles() {
-   return src('app/scss/style.scss')
-      .pipe(scss({ outputStyle: 'compressed' }).on('error', scss.logError))
-      .pipe(concat('style.min.css'))
-      .pipe(autoprefixer({
-         overrideBrowserslist: ['last 10 versions'],
-         grid: true
-      }))
-      .pipe(dest('app/css'))
-      .pipe(browserSync.stream())
+	return src('app/scss/style.scss')
+		.pipe(scss({ outputStyle: 'compressed' }).on('error', scss.logError))
+		.pipe(concat('style.min.css'))
+		.pipe(autoprefixer({
+			overrideBrowserslist: ['last 10 versions'],
+			grid: true
+		}))
+		.pipe(dest('app/css'))
+		.pipe(browserSync.stream())
 }
 
 function scripts() {
-   return src([
-      'node_modules/jquery/dist/jquery.js',
-      'app/js/main.js'
-   ])
-      .pipe(concat('main.min.js'))
-      .pipe(uglify())
-      .pipe(dest('app/js'))
-      .pipe(browserSync.stream())
+	return src([
+		'node_modules/jquery/dist/jquery.js',
+		'node_modules/slick-carousel/slick/slick.js',
+		'app/js/main.js'
+	])
+		.pipe(concat('main.min.js'))
+		.pipe(uglify())
+		.pipe(dest('app/js'))
+		.pipe(browserSync.stream())
 }
 
 function images() {
-   return src('app/images/**/*.*')
-      .pipe(imagemin([
-         imagemin.gifsicle({ interlaced: true }),
-         imagemin.mozjpeg({ quality: 75, progressive: true }),
-         imagemin.optipng({ optimizationLevel: 5 }),
-         imagemin.svgo({
-            plugins: [
-               { removeViewBox: true },
-               { cleanupIDs: false }
-            ]
-         })
-      ]))
-      .pipe(dest('dist/images'))
+	return src('app/images/**/*.*')
+		.pipe(imagemin([
+			imagemin.gifsicle({ interlaced: true }),
+			imagemin.mozjpeg({ quality: 75, progressive: true }),
+			imagemin.optipng({ optimizationLevel: 5 }),
+			imagemin.svgo({
+				plugins: [
+					{ removeViewBox: true },
+					{ cleanupIDs: false }
+				]
+			})
+		]))
+		.pipe(dest('dist/images'))
 }
 
 function build() {
-   return src([
-      'app/**/*.html',
-      'app/css/style.min.css',
-      'app/js/main.min.js'
-   ], { base: 'app' })
-      .pipe(dest('dist'))
+	return src([
+		'app/**/*.html',
+		'app/css/style.min.css',
+		'app/js/main.min.js'
+	], { base: 'app' })
+		.pipe(dest('dist'))
 }
 
 function cleanDist() {
-   return del('dist')
+	return del('dist')
 }
 
 function watching() {
-   watch(['app/scss/**/*.scss'], styles)
-   watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts)
-   watch(['app/**/*.html']).on('change', browserSync.reload)
+	watch(['app/scss/**/*.scss'], styles)
+	watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts)
+	watch(['app/**/*.html']).on('change', browserSync.reload)
 }
 
 exports.styles = styles;
